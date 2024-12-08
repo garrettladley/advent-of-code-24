416e3b22e6cda01fc26b041377be12354dd0f1ba
garrettladley <garrett.ladley14@gmail.com>
2024-12-08 18:06:22 -0500

    fix: clippy allow

--- rust/d05/src/p2.rs
+++ rust/d05/src/p2.rs
@@ -1,3 +1,6 @@
+// See https://github.com/rust-lang/rust-clippy/issues/13185
+// Using map on L25 is intentional as we need to mutate the Vec<u32> in place
+#[allow(clippy::manual_inspect)]
 use miette::miette;
 use nom::{
     bytes::complete::tag,
