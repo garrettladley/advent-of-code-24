b501f8eda8f1552a86213ee273eb3b65014fc42f
garrettladley <garrett.ladley14@gmail.com>
2024-12-08 17:52:35 -0500

    fix: rm tracing macros

--- rust/d04/src/bin/p1.rs
+++ rust/d04/src/bin/p1.rs
@@ -1,7 +1,6 @@
 use d04::p1::process;
 use miette::Context;
 
-#[tracing::instrument]
 fn main() -> miette::Result<()> {
     let file = include_str!("../../input.txt");
     let result = process(file).context("process part 1")?;
--- rust/d04/src/bin/p2.rs
+++ rust/d04/src/bin/p2.rs
@@ -1,7 +1,6 @@
 use d04::p2::process;
 use miette::Context;
 
-#[tracing::instrument]
 fn main() -> miette::Result<()> {
     let file = include_str!("../../input.txt");
     let result = process(file).context("process part 2")?;
